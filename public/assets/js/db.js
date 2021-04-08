034e46ba68d4ccce0fac1c3de61c3bd802fc928c
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-09 08:26:11 +1000

    save record function; readwrite access to pending db creates transaction and adds record

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -8,3 +8,26 @@ request.onupgradeneeded = function (event) {
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    // check if app is online before reading from db
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+
+    // add record to your store with add method.
+    store.add(record);
+}
